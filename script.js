7a9ca76b95cda10ef1522144f2303e9bfc1eb850
Amaya Erhabor <aerhabor2005@gmail.com>
2025-05-11 17:39:39 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const sections = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.1
+  }
+);
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
